fd7f5d94fb24ba391819b9c86b175c2beb79bc0a
Ryan Mulligan <rmulligan@inspirato.com>
2020-11-04 19:48:25 -0500

    Add card transitions and clean up code

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,92 +1,95 @@
 <template>
   <div class="card" :data-type="dataType">
-    <div class="type">{{type}}</div>
-    <div class="image">{{image}}</div>
-    <div class="description">{{description}}</div>
+    <div class="type">{{ type }}</div>
+    <div class="image">{{ image }}</div>
+    <div class="description">{{ description }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Card',
+  name: "Card",
   props: {
-    type: String,
-    image: String,
     description: String,
+    image: String,
+    type: String,
   },
 
   computed: {
     dataType() {
-      return this.type.replace(/\s+/g, '-').toLowerCase();
+      return this.type.replace(/\s+/g, "-").toLowerCase();
     },
   },
 };
 </script>
 
 <style scoped>
-  .card {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: var(--card-size);
-    height: calc(var(--card-size) * 1.3);
-    border: 2px solid black;
-    border-radius: 0.125rem;
-    background-color: whitesmoke;
-    overflow: hidden;
-  }
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: var(--card-size);
+  height: calc(var(--card-size) * 1.3);
+  border: 2px solid black;
+  border-radius: 0.125rem;
+  background-color: whitesmoke;
+  overflow: hidden;
+}
 
-  .type {
-    font-size: 1.2rem;
-    text-transform: uppercase;
-    padding: calc(var(--space) / 2) var(--space);
-    width: 100%;
-    text-align: center;
-  }
+.type {
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  padding: calc(var(--space) / 2) var(--space);
+  width: 100%;
+  text-align: center;
+}
 
-  .image {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    background-color: white;
-    overflow: hidden;
-  }
+.image {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-color: white;
+  overflow: hidden;
+}
 
-  .image::after {
-    content: "";
-    display: block;
-    padding-bottom: calc(100% / (16 / 9));
-  }
+.image::after {
+  content: "";
+  display: block;
+  padding-bottom: calc(100% / (16 / 9));
+}
 
-  .image img {
-    position: absolute;
-    object-fit: cover;
-  }
+.image img {
+  position: absolute;
+  object-fit: cover;
+}
 
-  .description {
-    flex: 1;
-    padding: var(--space);
-    font-size: 1rem;
-    line-height: 1.15;
-  }
+.description {
+  flex: 1;
+  padding: var(--space);
+  font-size: 1rem;
+  line-height: 1.15;
+}
 
-  /* Data types */
-  [data-type="one"] {
-    color: whitesmoke;
-    background-color: darkslategray;
-  }
+/* Data types */
+[data-type="rune"] {
+  color: whitesmoke;
+  background-color: darkslategray;
+}
 
-  [data-type="two"] {
-    color: black;
-    background-color: gold;
-  }
+[data-type="hp-potion"] {
+  color: whitesmoke;
+  background-color: crimson;
+}
 
-  [data-type="three"] {
-    color: whitesmoke;
-    background-color: crimson;
-  }
+[data-type="gold"] {
+  background-color: gold;
+}
+
+[data-type="fools-gold"] {
+  background-color: yellow;
+}
 </style>
--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,74 +1,88 @@
 <template>
   <div :id="id" class="deck-container u-flow">
     <div class="deck u-flex-center">
-      <div class="deck-name">{{id}}</div>
-      <Card
-        v-for="({ type, image, description }, index) in activeCards"
-        :key="index"
-        :type="type"
-        :image="image"
-        :description="description"
-        class="card"
-      />
+      <div class="deck-name">{{ id }}</div>
+      <transition-group name="card">
+        <Card
+          v-for="({ type, image, description }, index) in activeCards"
+          :key="index"
+          :type="type"
+          :image="image"
+          :description="description"
+          class="card"
+        />
+      </transition-group>
     </div>
-    <button
-      class="button"
-      @click="drawCard"
-      :disabled="activeCards.length == cards.length"
-    >
-      {{activeCards.length == cards.length ? 'Empty' : 'Draw'}}
+    <button class="button" @click="drawCard" :disabled="activeCards.length == cards.length">
+      {{ activeCards.length == cards.length ? "Empty" : "Draw" }}
     </button>
   </div>
 </template>
 
 <script>
-import Card from './Card.vue';
+import Card from "./Card.vue";
 
 export default {
-  name: 'Deck',
+  name: "Deck",
   components: { Card },
   props: {
     id: String,
-    cards: Array,
-  },
-
-  methods: {
-    drawCard() {
-      this.$emit('draw', this.id);
-    },
+    cards: Array
   },
 
   computed: {
     activeCards() {
-      return this.cards.filter((card) => card.active);
-    },
+      return this.cards.filter(card => card.active);
+    }
   },
+
+  methods: {
+    drawCard() {
+      this.$emit("draw", this.id);
+    }
+  }
 };
 </script>
 
 <style scoped>
-  .deck-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.deck-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .deck {
-    flex: 0 0 auto;
-    position: relative;
-    width: var(--card-size);
-    height: calc(var(--card-size) * 1.3);
-    border: 2px dashed var(--color-grayscale);
-  }
+.deck {
+  flex: 0 0 auto;
+  position: relative;
+  width: var(--card-size);
+  height: calc(var(--card-size) * 1.3);
+  border: 2px dashed var(--color-grayscale);
+  transform: translateZ(0);
+  transform-style: preserve-3d;
+}
 
-  .deck-name {
-    color: var(--color-grayscale);
-    text-transform: capitalize;
-  }
+.deck-name {
+  color: var(--color-grayscale);
+  text-transform: capitalize;
+}
 
-  .card {
-    position: absolute;
-    top: -2px;
-    left: -2px;
-  }
+.card {
+  position: absolute;
+  top: -2px;
+  left: -2px;
+}
+
+.card-enter-active {
+  transition: opacity var(--duration-sm) var(--ease-out),
+    transform var(--duration-md) var(--ease-out);
+}
+
+.card-enter-from {
+  opacity: 0;
+  transform: translateY(20%);
+}
+
+.button {
+  z-index: 1;
+}
 </style>
